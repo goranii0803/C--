62d1762cb0d376e2f3133fff7a8463d88f58f87b
goranii0803 <godgoranii@gmail.com>
2024-04-01 01:39:05 +0900

    commit_0

---
+++ synthetic_division.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     int a,b,c,d;
+     int i,u,v,w,x,y,z,j,k,l;
+     cout<<"Enter four number (not letter or alphabet) and root of equation\n";
+     cin>>a>>b>>c>>d>>i;
+     u=a;
+     v=i*u;
+     w=b+(v);
+     x=i*w;
+     y=c+x;
+     z=i*y;
+     j=d+z;
+     cout<<u<<" "<<w<<" "<<y<<" "<<j;
+
+     getch();
+     return 0;
+   
+}
